98f3850dcd4e3cb02cd14a56607a94bf8c4803a5
Ricky Moorhouse <ricky@samespirit.net>
2017-04-17 18:15:42 +0100

    Make setup rerunnable

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
+if [ ! -d backups ]; 
+then
+  mkdir backups
+fi
+
+for DOTFILE in bashrc bash_profile profile vim vimrc
+do
+  if [ -L ~/.$DOTFILE ]; 
+  then
+    echo ~/.$DOTFILE is a symlink
+    ls -la ~/.$DOTFILE
+  else
+    echo $DOTFILE not a symlink so replacing
+    mv ~/.$DOTFILE ~/dotfiles/backups/$DOTFILE
+    ln -s ~/dotfiles/$DOTFILE ~/.$DOTFILE
+  fi
+done
